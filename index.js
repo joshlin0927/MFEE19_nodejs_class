c3d55d97023a2f84330a4e67b875370415c43f4a
Josh Lin <joshlin0927@gmail.com>
2021-09-06 16:45:11 +0800

    lesson1

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('public'));
+
+
+// ***路由定義開始 ：BEGIN
 app.get('/', (req, res)=>{
     res.send('<h2>Hello</h2>')
 });
+
+// ***路由定義結束 ：END
+
+
+
+
+app.use((req, res)=>{
+    res.status(404).send(`<h1>找不到頁面</h1>`)
+})
+
+
 
 let port = process.env.PORT || 3000;
 app.listen(port, ()=>{
